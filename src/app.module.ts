a750c6911ee122808a8c5618e78069772354c9a3
Susmita Dhital <susmitadhital7@gmail.com>
2026-05-16 13:19:24 +1000

    prisma fixes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,16 @@
+import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+
+import { UsersModule } from './users/users.module';
+import { PrismaModule } from 'prisma/prisma.module';
 import { HealthModule } from './health/health.module';
 
 @Module({
-  imports: [HealthModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    PrismaModule,
+    HealthModule,
+    UsersModule,
+  ],
 })
 export class AppModule {}
